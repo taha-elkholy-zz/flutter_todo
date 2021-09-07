6cc13b91eff9305d1a47a8599205eafa7128353b
Taha Elkholy <53704459+taha-elkholy@users.noreply.github.com>
2021-09-07 20:20:07 +0200

    add conditional builder

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,3 +1,4 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/shared/components/components.dart';
@@ -60,103 +61,103 @@ class Home extends StatelessWidget {
                   scaffoldKey.currentState!
                       .showBottomSheet(
                         (context) {
-                          return Container(
-                            width: double.infinity,
-                            child: Form(
-                              key: formKey,
-                              child: Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      'Add New Task',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    defaultTextFormField(
-                                        controller: titleController,
-                                        validator: (value) {
-                                          if (value!.isEmpty) {
-                                            return 'Title is required';
-                                          }
-                                        },
-                                        inputType: TextInputType.text,
-                                        label: 'Task Title',
-                                        prefix: Icons.text_fields),
-                                    //defaultTextFormField
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    defaultTextFormField(
-                                        controller: timeController,
-                                        validator: (value) {
-                                          if (value!.isEmpty) {
-                                            return 'Time is required';
-                                          }
-                                        },
-                                        inputType: TextInputType.datetime,
-                                        label: 'Task Time',
-                                        prefix: Icons.watch_later_outlined,
-                                        onTap: () {
-                                          showTimePicker(
-                                                  context: context,
-                                                  initialTime: TimeOfDay.now())
-                                              .then((value) {
-                                            cubit.setTime(
-                                                context: context,
-                                                timeOfDay: value);
-                                            timeController.text = cubit.time;
-                                          });
-                                        }),
-                                    //defaultTextFormField
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    defaultTextFormField(
-                                        controller: dateController,
-                                        validator: (value) {
-                                          if (value!.isEmpty) {
-                                            return 'Date is required';
-                                          }
-                                        },
-                                        inputType: TextInputType.datetime,
-                                        label: 'Task Date',
-                                        prefix: Icons.calendar_today_outlined,
-                                        onTap: () {
-                                          showDatePicker(
-                                            context: context,
-                                            initialDate: DateTime.now(),
-                                            firstDate: DateTime.now(),
-                                            lastDate: DateTime.now()
-                                                .add(Duration(days: 365)),
-                                          ).then((value) {
-                                            cubit.setDate(value: value);
-                                            dateController.text = cubit.date;
-                                          });
-                                        }),
-                                    //defaultTextFormField
-                                  ],
+                      return Container(
+                        width: double.infinity,
+                        child: Form(
+                          key: formKey,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Add New Task',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                defaultTextFormField(
+                                    controller: titleController,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Title is required';
+                                      }
+                                    },
+                                    inputType: TextInputType.text,
+                                    label: 'Task Title',
+                                    prefix: Icons.text_fields),
+                                //defaultTextFormField
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                defaultTextFormField(
+                                    controller: timeController,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Time is required';
+                                      }
+                                    },
+                                    inputType: TextInputType.datetime,
+                                    label: 'Task Time',
+                                    prefix: Icons.watch_later_outlined,
+                                    onTap: () {
+                                      showTimePicker(
+                                          context: context,
+                                          initialTime: TimeOfDay.now())
+                                          .then((value) {
+                                        cubit.setTime(
+                                            context: context,
+                                            timeOfDay: value);
+                                        timeController.text = cubit.time;
+                                      });
+                                    }),
+                                //defaultTextFormField
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                defaultTextFormField(
+                                    controller: dateController,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Date is required';
+                                      }
+                                    },
+                                    inputType: TextInputType.datetime,
+                                    label: 'Task Date',
+                                    prefix: Icons.calendar_today_outlined,
+                                    onTap: () {
+                                      showDatePicker(
+                                        context: context,
+                                        initialDate: DateTime.now(),
+                                        firstDate: DateTime.now(),
+                                        lastDate: DateTime.now()
+                                            .add(Duration(days: 365)),
+                                      ).then((value) {
+                                        cubit.setDate(value: value);
+                                        dateController.text = cubit.date;
+                                      });
+                                    }),
+                                //defaultTextFormField
+                              ],
                             ),
-                          );
-                        },
-                        elevation: 25,
-                        backgroundColor: Colors.grey[100],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
-                      ) // BottomSheet
+                      );
+                    },
+                    elevation: 25,
+                    backgroundColor: Colors.grey[100],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ) // BottomSheet
                       .closed
                       .then((value) {
-                        // when close the bottom sheet
-                        // change the state of bottom sheet from open to closed
+                    // when close the bottom sheet
+                    // change the state of bottom sheet from open to closed
                         cubit.changeBottomSheetState(
                             isBottomSheetShown: false, fabIcon: Icons.edit);
                       });
@@ -167,7 +168,12 @@ class Home extends StatelessWidget {
                 }
               },
             ),
-            body: cubit.screens[cubit.currentIndex],
+            body: ConditionalBuilder(
+                condition: state is AppGetDatabaseLoadingState,
+                builder: (context) => Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                fallback: (context) => cubit.screens[cubit.currentIndex]),
             bottomNavigationBar: BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
               currentIndex: cubit.currentIndex,
